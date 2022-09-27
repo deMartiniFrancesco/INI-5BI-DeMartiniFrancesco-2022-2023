056bd0791d4d0735c4135097d6f54ac81f92081d
deMartiniFrancesco <18606@studenti.marconiverona.edu.it>
2022-09-27 13:40:21 +0200

    Jdbc update 2

--- src/demartini_F_Jdbc/bin/Jdbc.java
+++ src/demartini_F_Jdbc/bin/Jdbc.java
@@ -1,38 +1,106 @@
 package demartini_F_JDBC.bin;
 
 import java.io.File;
+import java.sql.*;
 
 class Jdbc {
+    static String LEZIONE = "lezione_deMartini";
+    static String DOCENTE = "docente_deMartini";
 
-        public Jdbc() {
+    private Connection connection;
 
+    public Jdbc() {
+        try {
+            connection = DriverManager.getConnection("jdbc:mysql://172.16.1.99:3306/db99999", "ut99999", "pw99999");
+            if (!notExistTable(connection, "lezioni_deMartini")) {
+                System.out.println("exist");
+                dropTable(connection, "lezioni_deMartini");
+            }
+            if (notExistTable(connection, LEZIONE)) {
+                createLezioiTable(connection);
+            }
+            if (notExistTable(connection, DOCENTE)) {
+                createDocenteTable(connection);
+            }
+
+        } catch (SQLException ex) {
+            // handle any errors
+            System.out.println("SQLException: " + ex.getMessage());
+            System.out.println("SQLState: " + ex.getSQLState());
+            System.out.println("VendorError: " + ex.getErrorCode());
         }
+
+
+    }
+
+    public static boolean notExistTable(Connection connection, String table) throws SQLException {
+        System.out.println("Jdbc.notExistTable");
+        DatabaseMetaData metaData = connection.getMetaData();
+        ResultSet resultSet = metaData.getTables(null, null, "%", null);
+        while (resultSet.next()) {
+            if (table.equalsIgnoreCase(resultSet.getString(3))) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+
+    public void dropTable(Connection connection, String table) throws SQLException {
+        Statement statement = connection.createStatement();
+        statement.execute("DROP TABLE " + table + ";");
+        System.out.println("Jdbc.dropTable");
+    }
+
+    public void createLezioiTable(Connection connection) throws SQLException {
+        Statement statement = connection.createStatement();
+        statement.executeUpdate("CREATE TABLE `" + LEZIONE + "` ("
+                + "  `id` bigint(20) NOT NULL primary key,"
+                + "  `aula` varchar(30) DEFAULT NULL,"
+                + "  `materia` varchar(20) NOT NULL,"
+                + "  `classe` varchar(20),"
+                + "  `giorno` int(20)  NOT NULL,"
+                + "  `ora` int(20)  NOT NULL"
+                + ") ENGINE=InnoDB DEFAULT CHARSET=latin1");
+        System.out.println("Jdbc.createLezioiTable");
+    }
+
+    public void createDocenteTable(Connection connection) throws SQLException {
+        Statement statement = connection.createStatement();
+        statement.executeUpdate("CREATE TABLE `" + DOCENTE + "` ("
+                + "  `id` bigint(20) NOT NULL primary key,"
+                + "  `nome_cognome` varchar(40) NOT NULL"
+                + ") ENGINE=InnoDB DEFAULT CHARSET=latin1");
+        System.out.println("Jdbc.createDocenteTable");
+    }
+
+
 }
 
 class JdbcTest {
-        public static void main(String[] args) {
+    public static void main(String[] args) {
 
-                System.out.println("Start");
+        System.out.println("Start");
 
-                //              CALCOLO PATH RELATIVO UNIVERSALE
-                //----------------------------------------------------------------------
-                String tempPath = new File(
-                                String.valueOf(Jdbc.class.getPackage()).replace("package ", "").replace(".", "/")  
-                ).getParent();
-                File uesrPath = new File(System.getProperty("user.dir"));
-                String projectPath = uesrPath.getName().equals(tempPath) ?
-                                uesrPath.getPath() :
-                                new File(uesrPath.getPath() + "/src").exists() ?
-                                                uesrPath.getPath() + "/src/" + tempPath :
-                                                uesrPath.getPath() + tempPath;
-                //----------------------------------------------------------------------
+        //              CALCOLO PATH RELATIVO UNIVERSALE
+        //----------------------------------------------------------------------
+        String tempPath = new File(
+                String.valueOf(Jdbc.class.getPackage()).replace("package ", "").replace(".", "/")
+        ).getParent();
+        File uesrPath = new File(System.getProperty("user.dir"));
+        String projectPath = uesrPath.getName().equals(tempPath) ?
+                uesrPath.getPath() :
+                new File(uesrPath.getPath() + "/src").exists() ?
+                        uesrPath.getPath() + "/src/" + tempPath :
+                        uesrPath.getPath() + tempPath;
+        //----------------------------------------------------------------------
 
-                // COSTANTI
-                String resursesPath = "/file/";
+        // COSTANTI
+        String resursesPath = "/file/";
 
-                System.out.println("Hello, World");
+        Jdbc jdbc = new Jdbc();
 
-                System.out.println("End");
+        System.out.println("End");
 
-        }
+    }
 }
