30abe216e352d28b0ed918b9fd9289fe6dfabf01
deMartiniFrancesco <18606@studenti.marconiverona.edu.it>
2022-11-08 13:02:59 +0100

    Example files

---
+++ src/main/java/deMartiniFrancesco/projects/demartini_F_PHP01/doc/README.md
@@ -0,0 +1,10 @@
+# Program name: PHP01.java
+
+---
+
+## Consegna
+Sull’ app di gestione alunni:
+-Creare un form di ricerca per la classe, la pagina di risposta deve visualizzare la classe in ordine alfabetico e
+il numero totale degli alunni.
+-Creare una pagina che permetta di modificare la classe di un alunno.
+-Creare una pagina che permetta, dopo un controllo input, di aggiungere un nuovo alunno.
---
+++ src/main/java/deMartiniFrancesco/projects/demartini_F_PHP01/file/visalunni.php
@@ -0,0 +1,53 @@
+<?php 
+include "libs/db_connect.php";
+?>
+<!DOCTYPE HTML>
+<html>
+    <head>
+        <meta charset="utf-8" />
+        <title> MYSQL PHP</title>
+		<link rel="stylesheet" href="style.css">
+		  <link rel="icon" type="image/x-icon" href="favicon.png">
+    </head>
+<body>
+<?php
+
+        $query = "SELECT nome,cognome,matricola,data_nascita,classe from alunni";
+        try {
+            $stmt = $con->prepare( $query );	// $con arriva da include 
+            $stmt->execute(array());
+            //Lettura numero righe risultato 
+            $num = $stmt->rowCount();
+          
+        } catch(PDOException $ex) {
+            print("Errore !".$ex->getMessage());
+        }
+          
+        print( "<table>");
+        print( "<tr>");
+        print( "<th> N</th>");
+        print( "<th> Matricola</th>");
+        print( "<th>nome</th>");
+		print( "<th>cognome</th>");
+		print( "<th>classe</th>");
+		print( "<th>D_Nasc</th>");
+        print( "</tr>");
+		$nal=1;
+        while ($row = $stmt->fetch(PDO::FETCH_ASSOC)){
+            print( "<tr>");
+
+            print( "<td>$nal</td>");
+            print( "<td>".$row['matricola']."</td>");
+            print( "<td>".$row['nome']."</td>");
+            print( "<td>".$row['cognome']."</td>");
+            print( "<td>".$row['classe']."</td>");
+            print( "<td>".$row['data_nascita']."</td>");
+
+            print( "</tr>");
+		    $nal++;
+        }
+        print( "</table>");
+    
+?> 
+</body>
+</html>
